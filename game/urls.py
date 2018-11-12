4d9ddba20dd126af65c2cea5d0ef0ea35dd6528b
katsu-kun <katsuyou.kun@gmail.com>
2018-11-12 00:17:03 -0500

    Added pages for the play and about

--- game/urls.py
+++ game/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('play', views.play, name='play'),
+    path('about', views.about, name='about')
 ]
--- game/views.py
+++ game/views.py
@@ -9,3 +9,15 @@ def index(request):
 
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
+
+def play(request):
+    context = {}
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'play.html', context=context)
+
+def about(request):
+    context = {}
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'about.html', context=context)
