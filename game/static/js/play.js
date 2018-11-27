7063441866d8f249f2073c37d2d575dc58499c5a
katsu-kun <katsuyou.kun@gmail.com>
2018-11-27 16:46:53 -0500

    All of the core functionality is implemented. That is to say, the backend is mostly done. The core of the work left is to improve funcionality by making the front end. To do this, I'll have to learn CSS and learn how to make things pretty with this. This will will cover the styling of the forms and should be able to change the styling to alert to user as to whether they are correct or not

---
+++ game/static/js/play.js
@@ -0,0 +1,13 @@
+$(document).ready( function() {
+  var form =$('#user-attempt');
+  var submitted_attempt=form.attr("attempt")
+
+  form.submit(function() {
+    $.ajax({
+      type:form.attr('method'),
+      url: form.attr('action')
+      data: {attempt:submitted_attempt}
+    });
+    return false;
+  });
+});
