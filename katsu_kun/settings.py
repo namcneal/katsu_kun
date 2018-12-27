e23c5b485a392ea13b1f01a07408c89d722a9728
katsu-kun <katsuyou.kun@gmail.com>
2018-12-27 00:53:32 -0500

    Updated settings

--- katsu_kun/settings.py
+++ katsu_kun/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '(dxgs*0tg%ym6g0*ftn(hdl%(wn3*@ers=*1gobxhv97#q*!o!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
