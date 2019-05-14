4b5d960b0c6d10fa285f7f3eee325f20670585ad
katsu-kun <katsuyou.kun@gmail.com>
2019-05-14 02:01:43 -0400

    Heroku Launch was successful, but still need to get static files working

--- katsu_kun/settings.py
+++ katsu_kun/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/    
 
 STATIC_ROOT = os.path.join(BASE_DIR, '/static')
-STATIC_URL = 'game/static/'    
+STATIC_URL = '/static/'    
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = [
