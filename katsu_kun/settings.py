db58d66fd97b53a8cd37b8d4dbedc9a8e0fc7494
katsu-kun <katsuyou.kun@gmail.com>
2019-05-13 22:15:19 -0400

    First push for version 1.00

--- katsu_kun/settings.py
+++ katsu_kun/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '(dxgs*0tg%ym6g0*ftn(hdl%(wn3*@ers=*1gobxhv97#q*!o!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS =  ['localhost', '127.0.0.1', '[::1]']
 
 
 # Application definition
