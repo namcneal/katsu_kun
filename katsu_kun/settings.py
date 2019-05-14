df7963592571ee13d46a028fcd8575d79b74b131
katsu-kun <katsuyou.kun@gmail.com>
2019-05-14 02:28:20 -0400

    Heroku Launch was successful, but still need to get static files working

--- katsu_kun/settings.py
+++ katsu_kun/settings.py
@@ -14,7 +14,7 @@ import os
 from django.conf import settings
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__) + "/game"))
 
 
 # Quick-start development settings - unsuitable for production
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    # 'django.contrib.staticfiles',
+    'django.contrib.staticfiles',
     'game'
 ]
 
@@ -124,12 +124,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/    
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'Wstatic')
-STATIC_URL = 'game/static/'    
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_URL = '/static/'    
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'game/static'),
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 # Simplified static file serving.
