b7413d76d2bf036fc718ffe933c0ef6421d68def
katsu-kun <katsuyou.kun@gmail.com>
2019-05-14 00:39:33 -0400

    Heroku Launch was successful, but still need to get static files working

--- game/urls.py
+++ game/urls.py
@@ -10,3 +10,8 @@ urlpatterns = [
 
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns += staticfiles_urlpatterns()
+
+if not settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    )
