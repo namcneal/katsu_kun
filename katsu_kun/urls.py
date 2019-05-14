dea0dceadccded867aee4b6912e5306c36289cf9
katsu-kun <katsuyou.kun@gmail.com>
2019-05-14 01:21:43 -0400

    Heroku Launch was successful, but still need to get static files working

--- katsu_kun/urls.py
+++ katsu_kun/urls.py
@@ -37,5 +37,5 @@ urlpatterns += [
     path('', RedirectView.as_view(url='/game/', permanent=True)),
 ]
 
-if settings.DEBUG is True:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
